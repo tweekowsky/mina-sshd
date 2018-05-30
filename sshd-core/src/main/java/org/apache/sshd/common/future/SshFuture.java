e56151b915e8dc787fb1af72bf927b91537e49c9
Guillaume Nodet <gnodet@apache.org>
2018-05-30 11:17:53 +0200

    Remove unused WaitableFuture

--- sshd-core/src/main/java/org/apache/sshd/common/future/SshFuture.java
+++ sshd-core/src/main/java/org/apache/sshd/common/future/SshFuture.java
@@ -18,15 +18,106 @@
  */
 package org.apache.sshd.common.future;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 /**
- * Represents the completion of an asynchronous SSH operation on a given object
- * (it may be an SSH session or an SSH channel).
+ * Represents an asynchronous operation which one can wait for its completion.
+ * <B>Note:</B> the only thing guaranteed is that if {@code true} is returned
+ * from one of the {@code awaitXXX} methods then the operation has completed.
+ * However, the <B>caller</B> has to determine whether it was a successful or
+ * failed completion.
  * Can be listened for completion using a {@link SshFutureListener}.
  *
  * @param <T> Type of future
  * @author <a href="mailto:dev@mina.apache.org">Apache MINA SSHD Project</a>
  */
-public interface SshFuture<T extends SshFuture> extends WaitableFuture {
+public interface SshFuture<T extends SshFuture> {
+
+    /**
+     * @return Some identifier useful as {@link #toString()} value
+     */
+    Object getId();
+
+    /**
+     * Wait {@link Long#MAX_VALUE} msec. for the asynchronous operation to complete.
+     * The attached listeners will be notified when the operation is
+     * completed.
+     *
+     * @return {@code true} if the operation is completed.
+     * @throws IOException if failed - specifically {@link java.io.InterruptedIOException}
+     *                     if waiting was interrupted
+     * @see #await(long)
+     */
+    default boolean await() throws IOException {
+        return await(Long.MAX_VALUE);
+    }
+
+    /**
+     * Wait for the asynchronous operation to complete with the specified timeout.
+     *
+     * @param timeout   The number of time units to wait
+     * @param unit      The {@link TimeUnit} for waiting
+     * @return {@code true} if the operation is completed.
+     * @throws IOException if failed - specifically {@link java.io.InterruptedIOException}
+     *                     if waiting was interrupted
+     * @see #await(long)
+     */
+    default boolean await(long timeout, TimeUnit unit) throws IOException {
+        return await(unit.toMillis(timeout));
+    }
+
+    /**
+     * Wait for the asynchronous operation to complete with the specified timeout.
+     *
+     * @param timeoutMillis Wait time in milliseconds
+     * @return {@code true} if the operation is completed.
+     * @throws IOException if failed - specifically {@link java.io.InterruptedIOException}
+     *                     if waiting was interrupted
+     */
+    boolean await(long timeoutMillis) throws IOException;
+
+    /**
+     * Wait {@link Long#MAX_VALUE} msec. for the asynchronous operation to complete
+     * uninterruptibly. The attached listeners will be notified when the operation is
+     * completed.
+     *
+     * @return {@code true} if the operation is completed.
+     * @see #awaitUninterruptibly(long)
+     */
+    default boolean awaitUninterruptibly() {
+        return awaitUninterruptibly(Long.MAX_VALUE);
+    }
+
+    /**
+     * Wait for the asynchronous operation to complete with the specified timeout
+     * uninterruptibly.
+     *
+     * @param timeout   The number of time units to wait
+     * @param unit      The {@link TimeUnit} for waiting
+     * @return {@code true} if the operation is completed.
+     * @see #awaitUninterruptibly(long)
+     */
+    default boolean awaitUninterruptibly(long timeout, TimeUnit unit) {
+        return awaitUninterruptibly(unit.toMillis(timeout));
+    }
+
+    /**
+     * Wait for the asynchronous operation to complete with the specified timeout
+     * uninterruptibly.
+     *
+     * @param timeoutMillis Wait time in milliseconds
+     * @return {@code true} if the operation is finished.
+     */
+    boolean awaitUninterruptibly(long timeoutMillis);
+
+    /**
+     * @return {@code true} if the asynchronous operation is completed. <B>Note:</B>
+     * it is up to the <B>caller</B> to determine whether it was a successful or
+     * failed completion.
+     */
+    boolean isDone();
+
     /**
      * Adds an event <tt>listener</tt> which is notified when
      * this future is completed. If the listener is added
